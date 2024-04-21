f7341f8795daaf63fc76e6e272aef0111203ebb5
iampatryk <mpatrykmiller@gmail.com>
2024-04-21 13:33:28 +0200

    DeckOfCardsAPI some changes

--- src/main/java/org/example/DeckOfCardsAPI.java
+++ src/main/java/org/example/DeckOfCardsAPI.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,38 +12,43 @@ public class DeckOfCardsAPI {
     private final String adsressURL = "https://deckofcardsapi.com/api/deck/";
     private final HttpClient client = HttpClient.newHttpClient();
 
+    public HttpResponse<String> getShuffledDecks(int deckCount) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(adsressURL + "new/shuffle/?deck_count=" + deckCount))
+                    .build();
 
-    public HttpResponse getShuffledDecks(int deckCount) throws IOException, InterruptedException {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(adsressURL + "new/shuffle/?deck_count=" + deckCount))
-                .build();
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response;
 
-        System.out.println(response.statusCode());
-        System.out.println(response.body());
-
-        return response;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public HttpResponse drawCard(String deckID, int count) throws IOException, InterruptedException {
+    public HttpResponse<String> drawCard(String deckID, int count) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(adsressURL + deckID +"/draw/?count=" + count))
+                    .build();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(adsressURL + deckID +"/draw/?count=" + count))
-                .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
 
-        System.out.println(response.statusCode());
-        System.out.println(response.body());
-
-        return response;
-    }
-
-    public void setHttpClient(HttpClient httpClient) {
+            return response;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
 
